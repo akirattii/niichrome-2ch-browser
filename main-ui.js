ebdf10926101ca3c222455874fe06bbe39226376
akirattii <tanaka.akira.2006@gmail.com>
2014-12-15 16:04:03 +0900

    v0.13.7

--- main-ui.js
+++ main-ui.js
@@ -1,7 +1,7 @@
 /**
  * niichrome 2ch browser
  *
- * @version 0.13.5
+ * @version 0.13.6
  * @author akirattii <tanaka.akira.2006@gmail.com>
  * @license The MIT License
  * @copyright (c) akirattii
